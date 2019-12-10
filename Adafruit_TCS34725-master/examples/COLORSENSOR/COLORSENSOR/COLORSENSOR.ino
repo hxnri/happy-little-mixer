3e8021ea97b0f6a6185a27a9f4d125a43d80ad3b
priyakatt <35713637+priyakatt@users.noreply.github.com>
2019-12-10 18:09:51 -0500

    Update COLORSENSOR.ino

--- Adafruit_TCS34725-master/examples/COLORSENSOR/COLORSENSOR/COLORSENSOR.ino
+++ Adafruit_TCS34725-master/examples/COLORSENSOR/COLORSENSOR/COLORSENSOR.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
 
+
 /* Modified from example code for the Adafruit TCS34725 breakout library */
 
 /* Connect SCL    to analog 5
@@ -17,10 +18,24 @@
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
 //global variables used to compare values
-uint16_t oldR, oldG, oldB;
+uint16_t oldR, oldG, oldB, is_ready;
+
+
+char receivedChar;
+int receivedInt = 0;
+boolean newData = false;
+
+void recvOneChar() {
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read();
+    //Serial.println(receivedChar);
+    newData = true;
+  }
+}
+
 
 void setup(void) {
-  Serial.begin(38400);
+  Serial.begin(9600);
 
   if (tcs.begin()) {
     Serial.println("Found sensor");
@@ -32,71 +47,72 @@ void setup(void) {
   oldR = 0;
   oldG = 0;
   oldB = 0;
+  is_ready = 0;
   // Now we're ready to get readings!
 }
 
 void loop(void) {
-  //r,g,b are the raw ADC values, range from 0-65535
-  //max is used to scale those values if they are too big
-  //R,G,B are the final values that will be sent back to the PIC32
-  uint16_t r, g, b, c, MAX, R, G, B, len_StringRGB;
-  //raw values scaled from 0-2550
-  float scaledR, scaledG, scaledB;
-
-  String StringR, StringG, StringB, StringRGB;
-
-  tcs.getRawData(&r, &g, &b, &c);
-
-  scaledR = ((float)r / 256) * 10;
-  scaledG = ((float)g / 256) * 10;
-  scaledB = ((float)b / 256) * 10;
-
-  if (scaledR > 255 | scaledG > 255 | scaledB > 255) {
-    MAX = scaledR;
-    if (scaledG > MAX) {
-      MAX = scaledG;
+  while (is_ready == 0)
+  {
+    recvOneChar();
+    if (receivedChar == 's') {
+      is_ready = 1;
     }
-    if (scaledB > MAX) {
-      MAX = scaledB;
+  }
+  while (is_ready)
+  {
+    //r,g,b are the raw ADC values, range from 0-65535
+    //max is used to scale those values if they are too big
+    //R,G,B are the final values that will be sent back to the PIC32
+    uint16_t r, g, b, c, MAX, R, G, B, len_StringRGB;
+    //raw values scaled from 0-2550
+    float scaledR, scaledG, scaledB;
+
+    String StringR, StringG, StringB, StringRGB;
+
+    tcs.getRawData(&r, &g, &b, &c);
+
+    scaledR = ((float)r / 256) * 10;
+    scaledG = ((float)g / 256) * 10;
+    scaledB = ((float)b / 256) * 10;
+
+    if (scaledR > 255 | scaledG > 255 | scaledB > 255) {
+      MAX = scaledR;
+      if (scaledG > MAX) {
+        MAX = scaledG;
+      }
+      if (scaledB > MAX) {
+        MAX = scaledB;
+      }
+      // scale the values to integer 0-255
+      R = 255 * scaledR / MAX;
+      G = 255 * scaledG / MAX;
+      B = 255 * scaledB / MAX;
     }
-    // scale the values to integer 0-255
-    R = 255 * scaledR / MAX;
-    G = 255 * scaledG / MAX;
-    B = 255 * scaledB / MAX;
-  }
-  else {
-    //values are already between 0-255, convert to integer
-    R = scaledR;
-    G = scaledG;
-    B = scaledB;
-  }
+    else {
+      //values are already between 0-255, convert to integer
+      R = scaledR;
+      G = scaledG;
+      B = scaledB;
+    }
 
-  //compare to previously scanned value, to ensure an accurate value is sent to PIC32
-  if (oldR == R && oldG == G && oldB == B) {
-    //Serial.print("R: "); Serial.print(R, DEC); Serial.print(" ");
-    //Serial.print("G: "); Serial.print(G, DEC); Serial.print(" ");
-    //Serial.print("B: "); Serial.print(B, DEC); Serial.print(" ");
-    //Serial.println(" ");
+    //compare to previously scanned value, to ensure an accurate value is sent to PIC32
+    if (oldR == R && oldG == G && oldB == B) {
+      StringR = String(R);
+      StringG = String(G);
+      StringB = String(B);
 
-    StringR = String(R);
-    StringG = String(G);
-    StringB = String(B);
-    
-    StringRGB = StringR + "," + StringG + "," + StringB;
-    len_StringRGB = StringRGB.length();
-    
-    Serial.println(StringRGB);
-    
-        
-    while (1);
-  }
-  else {
-    Serial.println("Scanning");
-  }
-  oldR = R;
-  oldG = G;
-  oldB = B;
+      StringRGB = StringR + "," + StringG + "," + StringB;
+      Serial.print(StringRGB);
+      Serial.println(" ");
+      while (1);
+    }
+    else {
+      //scanning
+    }
+    oldR = R;
+    oldG = G;
+    oldB = B;
 
+  }
 }
-
-
